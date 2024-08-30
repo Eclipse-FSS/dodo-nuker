38b09427403f1b87bf5d5acfb34ddaa812dc0420
sempiller <110603660+Sempiller@users.noreply.github.com>
2024-08-30 14:49:31 +0300

    Create src.py

---
+++ src.py
@@ -0,0 +1,60 @@
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
+# Source dropping at 20 Stars
+# If you want the source early, buy it from @kentuckypapi on discord
+# ----------------------------------------------------------------------
